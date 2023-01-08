80a7e258c1d168f19fca109f10969537a161beb3
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2023-01-08 12:22:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import streamlit as st
 import numpy as np 
 st.title("Notion Calculators for Choice and Optimal Input Demands")
 
-"Output"
-y = st.number_input("y")
+
+y = st.number_input("Output")
+a = st.slider("Good 1 preference", 0, 1)
+b = st.slider("Good 2 preference", 0, 1)
+e1 = st.number_input("Endowment of good 1")
+e2 = st.number_input("Endowment of good 2")
+alpha = st.slider("Preference factor (Alpha)", 0, 1)
+
+q_1^*=\cfrac{a(p_1e_1+p_2e_2)}{p_1}
+
+f"Optimal q1: {(a*(p1*e1+e2*p2))/p1}"
+f"Optimal q2: {((1-a)(p1*e2+p2*e2))/p2}"
