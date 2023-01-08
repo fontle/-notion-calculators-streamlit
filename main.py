78e05849769baf1367812040e13589da7aa23834
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2023-01-08 12:23:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ e1 = st.number_input("Endowment of good 1")
 e2 = st.number_input("Endowment of good 2")
 alpha = st.slider("Preference factor (Alpha)", 0, 1)
 
-q_1^*=\cfrac{a(p_1e_1+p_2e_2)}{p_1}
+st.latex("q_1^*=\cfrac{a(p_1e_1+p_2e_2)}{p_1}")
 
 f"Optimal q1: {(a*(p1*e1+e2*p2))/p1}"
 f"Optimal q2: {((1-a)(p1*e2+p2*e2))/p2}"
