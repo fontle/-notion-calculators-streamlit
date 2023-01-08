21e37c4b739b87f34055b4e45837650a44f20f79
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2023-01-08 12:34:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import streamlit as st
 import numpy as np 
 st.title("Cobb-Douglas Gross Demand Calculator")
 
-
-y = st.number_input("Output")
 col1,col2 = st.columns(2)
-
 p1= col1.number_input("Price of good 1", 0.01)
 p2 = col2.number_input("Price of good 2", 0.01)
 e1 = col1.number_input("Endowment of good 1")
 e2 = col2.number_input("Endowment of good 2")
 a = st.slider("Preference factor (a)", 0, 1)
-
 st.latex("q_1^*=\cfrac{a(p_1e_1+p_2e_2)}{p_1}")
-
+col1,col2 = st.columns(2)
 col1.write(f"Optimal q1: {(a*(p1*e1+e2*p2))/p1}")
 col2.write(f"Optimal q2: {((1-a)(p1*e2+p2*e2))/p2}")
