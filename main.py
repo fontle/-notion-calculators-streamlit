1f37612e7eeab990a9c7dc334cc7325d617484b7
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2023-01-08 12:39:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ p1= col1.number_input("Price of good 1", 0.01)
 p2 = col2.number_input("Price of good 2", 0.01)
 e1 = col1.number_input("Endowment of good 1")
 e2 = col2.number_input("Endowment of good 2")
-a = st.slider("Preference factor (a)", 0, 1)
+a = st.slider("Preference factor (a)", 0, 1, 0.01)
 
 col1,col2 = st.columns(2)
 col1.latex("q_1^*=\cfrac{a(p_1e_1+p_2e_2)}{p_1}")
