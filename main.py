825fc686e1170b389aa4b8927b6c01f5618ecb16
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2023-01-08 12:26:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ y = st.number_input("Output")
 col1,col2 = st.columns(2)
 
 a = col1.number_input("Good 1 preference", 0, 1)
-b = col2.st.number_input("Good 2 preference", 0, 1)
+b = col2.number_input("Good 2 preference", 0, 1)
 e1 = col1.number_input("Endowment of good 1")
 e2 = col2.number_input("Endowment of good 2")
 alpha = st.slider("Preference factor (Alpha)", 0, 1)
