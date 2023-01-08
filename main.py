6a7178c2fa7f170aa1f3b2ff979f7ac34a4b185e
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2023-01-08 12:28:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ st.title("Notion Calculators for Choice and Optimal Input Demands")
 y = st.number_input("Output")
 col1,col2 = st.columns(2)
 
-a = col1.number_input("Good 1 preference", 0, 1)
-b = col2.number_input("Good 2 preference", 0, 1)
+p1= col1.number_input("Price of good 1")
+p2 = col2.number_input("Price of good 2")
 e1 = col1.number_input("Endowment of good 1")
 e2 = col2.number_input("Endowment of good 2")
-alpha = st.slider("Preference factor (Alpha)", 0, 1)
+a = st.slider("Preference factor (a)", 0, 1)
 
 st.latex("q_1^*=\cfrac{a(p_1e_1+p_2e_2)}{p_1}")
 
